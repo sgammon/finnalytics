47cc779ab2c6c5597077c10af6f4fdfd625e45d4
Sam Gammon <sam.gammon@causes.com>
2014-06-28 22:40:17 -0700

    Add initial structure for logic.

---
+++ finnalytics/logic/security.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+  security logic
+  ~~~~~~~~~~~~~~
+
+  :author: Sam Gammon <sam@momentum.io>
+  :author: Ian Weisberger <ian@momentum.io>
+  :author: David Rekow <david@momentum.io>
+  :copyright: (c) momentum labs, 2014
+  :license: The inspection, use, distribution, modification or implementation
+            of this source code is governed by a private license - all rights
+            are reserved by the Authors (collectively, "momentum labs") and held
+            under relevant California and US Federal Copyright laws. For full
+            details, see ``LICENSE.md`` at the root of this project. Continued
+            inspection of this source code demands agreement with the included
+            license and explicitly means acceptance to these terms.
+
+'''
